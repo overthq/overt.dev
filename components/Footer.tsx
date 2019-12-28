5aaa0448f9e3159f2d87e0f011c632723d80d4d2
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-12-28 05:07:29 +0100

    Begin working on Footer content - while copywriting is being worked on

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FaGithub, FaTwitter, FaDiscord } from 'react-icons/fa';
 
 const FooterContainer = styled.footer`
 	background-color: #000000;
-	padding: 20px;
+	padding: 100px 20px;
 	display: flex;
+	flex-wrap: wrap;
 	justify-content: space-around;
 `;
 
@@ -14,12 +16,75 @@ const Logo = styled.img`
 	filter: invert(1);
 `;
 
+const footerSections = [
+	{
+		title: 'Product'
+	},
+	{
+		title: ''
+	},
+	{
+		title: ''
+	}
+];
+
+const SocialLink = styled.a`
+	color: #ffffff;
+	&:visited {
+		color: #ffffff;
+	}
+`;
+
+const links = [
+	{
+		icon: FaGithub,
+		href: 'https://github.com/overthq'
+	},
+	{
+		icon: FaTwitter,
+		href: 'https://twitter.com/overt_hq'
+	},
+	{
+		icon: FaDiscord,
+		href: 'https://discord.gg'
+	}
+];
+
+const SocialLinksContainer = styled.div`
+	display: flex;
+	color: #ffffff;
+	& > *:not(last-of-type) {
+		margin-right: 10px;
+	}
+`;
+
+const SocialLinks = () => (
+	<SocialLinksContainer>
+		{links.map(({ icon, href }, index) => (
+			<SocialLink
+				key={index}
+				href={href}
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				{React.createElement(icon, { size: 24 })}
+			</SocialLink>
+		))}
+	</SocialLinksContainer>
+);
+
 const Footer = () => (
 	<FooterContainer>
 		<div>
 			<Logo src='/static/images/overtlogo.svg' alt='Overt Logo' />
 			<p style={{ color: '#FFFFFF', textAlign: 'center' }}>&copy; 2020</p>
 		</div>
+		{footerSections.map(({ title }, index) => (
+			<div key={index}>
+				<h5 style={{ color: '#FFFFFF' }}>{title}</h5>
+			</div>
+		))}
+		<SocialLinks />
 	</FooterContainer>
 );
 
