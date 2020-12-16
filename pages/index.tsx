98ae5d62ea1d46165194ab33c940e9a026761166
Oluwakorede Fashokun <koredefashokun@gmail.com>
2020-12-16 05:30:24 +0100

    Update information on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,11 +28,14 @@ const Home = () => (
 				margin: 20px 0;
 			}
 
-			.link {
+			a {
 				text-decoration: none;
 				border-bottom: 1px dotted #505050;
 				color: inherit;
 				line-height: 18px;
+			}
+
+			.link {
 				margin: 0 15px;
 			}
 		`}</style>
@@ -40,21 +43,31 @@ const Home = () => (
 			<title>Overt | The future is open-source.</title>
 		</Head>
 		<p>
-			Open-source software powers a large amount of the world's technology. From
-			operating systems to self-driving car software, open-source code makes
-			technology accessible to everyone.
+			In today's world, most innovative software is hidden as proprietary
+			software, to ensure that it maintains its monopolistic value to its
+			creators, or the corporation behind it. This is understandable, since
+			software is valuable, and this is the preferred business model for most
+			software-based corporations. However, it also stifles collaboration,
+			transparency and innovation.
 		</p>
 		<p>
-			Proprietary software stands in the way of the collaboration and innovation
-			that is undoubtedly required to solve humanity's problems. We believe that
-			the solving these problems is not a task that can be accomplished by a
-			single entity.
+			A great number of the advances in software over the past decades would
+			have been impossible if not for the power of open source software.
+			Projects like <a href='https://linux.org'>Linux</a> and{' '}
+			<a href='https://git-scm.com'>Git</a>, which power a large number of
+			software systems are testaments to this fact. In fact, hardly any code
+			would be tangible, without the solid foundation built by open-source
+			software, as virtually every mainstream programming language is
+			open-source. Building in the open provides access to contributions from
+			people from all over the world, who bring their different perspectives and
+			experiences to create powerful solutions to important problems.
 		</p>
 		<p>
-			At Overt, we are building open-source software to solve problems we face
-			everyday. We believe in the collaboration of developers around the globe,
-			to build the software that will shape tomorrow. We are building software
-			for the the people, by the people.
+			Overt is a project that aims to create communities around building
+			open-source software to solve important and difficult real-world problems.
+			Its main premise is the belief that the collaboration of developers around
+			the globe, will produce software that will shape the future. We are
+			building the future, in the open.
 		</p>
 		<div className='links-container'>
 			{Object.entries(links).map(([name, link]) => (
